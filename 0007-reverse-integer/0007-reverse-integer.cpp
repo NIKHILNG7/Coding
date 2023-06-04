865a79abd9f7f7812b541579622ae07493531eb0
NIKHILNG7 <117136669+NIKHILNG7@users.noreply.github.com>
2023-06-05 00:13:33 +0530

    Time: 3 ms (53.24%), Space: 6.1 MB (15.68%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int reverse(long x) {
+        
+        if(x<=9 && x>=0)return x;
+        
+        if(x>-10 && x<0) return x;
+        
+        if(x<INT_MIN)return 0;
+        int value=0;
+       long long digit=0;
+        
+        
+        if(x<0){
+            
+            x=-x;
+            value=-1;
+            
+        }
+        else value=1;
+        
+        
+        while (x>=1){
+            
+            
+            digit=digit*10 + x%10;
+            x=x/10;
+            
+            
+        }
+        
+        long long k=value*digit;
+        
+        if(k<INT_MIN || k>INT_MAX)return 0;
+        
+        
+        return k;
+        
+        
+    }
+};
